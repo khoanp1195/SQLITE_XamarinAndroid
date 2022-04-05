b3ca02963ada649f982ec3861d87baf25b694662
Khoa dep trai <khoa.np1195@sinhvien.hoasen.edu.vn>
2022-04-05 22:22:06 +0700

    Sqlite

---
+++ sqlite_xamarinandroid/MainActivity.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using System.Collections.Generic;
+
+using Android.Util;
+
+namespace sqlite_xamarinandroid
+{
+    [Activity(Label = "AndroidSQLite", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ListView lstData;
+        List<Person> lstSource = new List<Person>();
+        DataBase db;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Create DataBase
+            db = new DataBase();
+            db.createDataBase();
+            string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            Log.Info("DB_PATH", folder);
+
+            lstData = FindViewById<ListView>(Resource.Id.listView);
+
+            var edtName = FindViewById<EditText>(Resource.Id.edtName);
+            var edtAge = FindViewById<EditText>(Resource.Id.edtAge);
+            var edtEmail = FindViewById<EditText>(Resource.Id.edtEmail);
+
+            var btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            var btnEdit = FindViewById<Button>(Resource.Id.btnEdit);
+            var btnDelete = FindViewById<Button>(Resource.Id.btnDelete);
+
+            //LoadData
+            LoadData();
+
+            //Event
+            btnAdd.Click += delegate
+            {
+                Person person = new Person() {
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.insertIntoTablePerson(person);
+                LoadData();
+            };
+
+            btnEdit.Click += delegate {
+                Person person = new Person()
+                {
+                    Id=int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.updateTablePerson(person);
+                LoadData();
+            };
+
+            btnDelete.Click += delegate {
+                Person person = new Person()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.deleteTablePerson(person);
+                LoadData();
+            };
+
+            lstData.ItemClick += (s,e) =>{
+                //Set background for selected item
+                for(int i = 0; i < lstData.Count; i++)
+                {
+                    if (e.Position == i)
+                        lstData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.DarkGray);
+                    else
+                        lstData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+                }
+
+                //Binding Data
+                var txtName = e.View.FindViewById<TextView>(Resource.Id.textView1);
+                var txtAge = e.View.FindViewById<TextView>(Resource.Id.textView2);
+                var txtEmail = e.View.FindViewById<TextView>(Resource.Id.textView3);
+
+                edtName.Text = txtName.Text;
+                edtName.Tag = e.Id;
+
+                edtAge.Text = txtAge.Text;
+
+                edtEmail.Text = txtEmail.Text;
+
+            };
+
+        }
+
+        private void LoadData()
+        {
+            lstSource = db.selectTablePerson();
+            var adapter = new ListViewAdapter(this, lstSource);
+            lstData.Adapter = adapter;
+        }
+    }
+}
---
+++ sqlite_xamarinandroid/obj/Debug/110/android/src/crc647ce131978329142e/ViewHolder.java
@@ -0,0 +1,38 @@
+package crc647ce131978329142e;
+
+
+public class ViewHolder
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("sqlite_xamarinandroid.ViewHolder, sqlite_xamarinandroid", ViewHolder.class, __md_methods);
+	}
+
+
+	public ViewHolder ()
+	{
+		super ();
+		if (getClass () == ViewHolder.class)
+			mono.android.TypeManager.Activate ("sqlite_xamarinandroid.ViewHolder, sqlite_xamarinandroid", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
